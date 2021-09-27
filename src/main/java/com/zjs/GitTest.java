e667a23ac74e0912b50a645700737e591f248e1c
zjs <1447159141@qq.com>
2021-09-27 17:58:40 +0800

    first commit

---
+++ src/main/java/com/zjs/GitTest.java
@@ -0,0 +1,14 @@
+package com.zjs;
+
+/**
+ * @Title GitTest
+ * @Author 帅总
+ * @Description 测试git
+ * @Date 2021-09-27 17:33
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
