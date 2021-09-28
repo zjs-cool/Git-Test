9d32dc6be3a0fca6f21501c6a55fab9926a877a3
zjs <1447159141@qq.com>
2021-09-28 17:05:44 +0800

    hot-fix commit1

--- src/main/java/com/zjs/GitTest.java
+++ src/main/java/com/zjs/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git1!");
         System.out.println("hello git1!");
+        System.out.println("hello git4!");
     }
 }
