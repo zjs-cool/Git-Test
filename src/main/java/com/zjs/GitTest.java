a5a902e9fc02cfda2b83f9ecb3b1b4b229dfcc7d
zjs <1447159141@qq.com>
2021-10-11 21:20:07 +0800

    gitHub commit10

--- src/main/java/com/zjs/GitTest.java
+++ src/main/java/com/zjs/GitTest.java
@@ -18,7 +18,6 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("push test1!");
-        System.out.println("gitee pull2!");
-        System.out.println("gitee pull3!");
+        System.out.println("gitee pull!");
     }
 }
