4b907f0e1d96dbd169f25757632e1e6a4ad5f39a
zjs <1447159141@qq.com>
2021-10-12 20:23:42 +0800

    gitHub huiGun5

--- src/main/java/com/zjs/GitTest.java
+++ src/main/java/com/zjs/GitTest.java
@@ -22,8 +22,5 @@ public class GitTest {
         System.out.println("push test1!");
         System.out.println("gitee pull!");
         System.out.println("gitHub 回滚1!");
-        System.out.println("gitHub 回滚2!");
-        System.out.println("gitHub 回滚3!");
-        System.out.println("gitHub 回滚4!");
     }
 }
